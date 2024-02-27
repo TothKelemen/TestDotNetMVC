02d140ed9bb950852092f1c02879e51f618aeda5
TothKelemen <tothkelemen@gmail.com>
2024-02-27 10:27:22 +0100

    [KEL-1] base models added

---
+++ WebApplication1/Models/BookPublisher.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication1.Models
+{
+    public class BookPublisher
+    {
+        public int Id { get; set; }
+
+        public string PublisherName { get; set; }
+
+        public List<Books> BookName { get; set; }
+    }
+}
---
+++ WebApplication1/Models/KellyBookUser.cs
@@ -0,0 +1,17 @@
+﻿namespace WebApplication1.Models
+{
+    public class KellyBookUser
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateOnly Birth {  get; set; }
+
+        public string Email { get; set; }
+
+        public List<Books> FavoriteBook { get; set; }
+
+        public List<BookPublisher> FavoritePublisher { get; set; }
+    }
+}
